7aa6607b53cb4c7f6dbc6082d895f0da2323418a
echoeoe <61526855+echoeoe@users.noreply.github.com>
2023-03-21 17:21:44 -0400

    Update converter.js
    added inch to cm conversion

--- converter.js
+++ converter.js
@@ -1,15 +1,29 @@
-function cmToIn(cm){
-    //in cm, out inches
+function cmToIn(cm){ //in cm, out inches
     var inches = cm / 2.54;
     return inches;
+}
+
+function inToCm(inches){
+    var cm = inches * 2.54;
+    return cm;
 }
 
 //event listener CM KEYUP
 document.getElementById('cmTfield').addEventListener("keyup", fromCm);
 
-function fromCm(){ 
-    //in cm, out inches 
+function fromCm(){ //cm input => display change 
     var cm = document.getElementById('cmTfield').value; //in display cm
     var inches = cmToIn(cm); //method calculate
     document.getElementById('inTfield').value = inches; //out display in
 }
+
+//event listener IN KEYUP
+document.getElementById('inTfield').addEventListener("keyup", fromIn);
+
+function fromIn(){ //inches input => display change
+    var inches = document.getElementById('inTfield').value;
+    var cm = inToCm(inches); 
+    document.getElementById('cmTfield').value = cm; 
+}
+
+exports = {cmToIn, inToCm};
