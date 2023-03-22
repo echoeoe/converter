ea6c91cd9633ac8b2909c085b582502a768cd52f
echoeoe <61526855+echoeoe@users.noreply.github.com>
2023-03-21 22:21:55 -0400

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,3 +1,4 @@
+// calculation functions
 function cmToIn(cm){ //in cm, out inches
     var inches = cm / 2.54;
     return inches;
@@ -6,6 +7,16 @@ function cmToIn(cm){ //in cm, out inches
 function inToCm(inches){
     var cm = inches * 2.54;
     return cm;
+}
+
+function mToFt(m){
+    var ft = m * 3.280839895;
+    return ft;
+}
+
+function feetToMeter(feet){
+    var meter = feet * 0.3048006096;
+    return meter;
 }
 
 //event listener CM KEYUP
@@ -24,4 +35,22 @@ function fromIn(){ //inches input => display change
     var inches = document.getElementById('inchInp').value;
     var cm = inToCm(inches); 
     document.getElementById('cmInp').value = cm; 
+}
+
+//event listener METER KEYUP
+document.getElementById('meterInp').addEventListener("keyup", fromM);
+
+function fromM(){
+    var m = document.getElementById('meterInp').value;
+    var ft = mToFt(m);
+    document.getElementById('feetInp').value = ft;
+}
+
+//event listener FEET KEYUP
+document.getElementById('feetInp').addEventListener("keyup", fromFeet);
+
+function fromFeet(){
+    var ft = document.getElementById('feetInp').value;
+    var m = feetToMeter(ft);
+    document.getElementById('meterInp').value = m;
 }
