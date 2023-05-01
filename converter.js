9b920a5ed4d0331b809116111bf8873de8ef8799
Lina Li <61526855+echoeoe@users.noreply.github.com>
2023-05-01 15:03:00 -0400

    Update converter.js
    revert to stage without breaking changes

--- converter.js
+++ converter.js
@@ -32,22 +32,13 @@ function meterToFtIn(meter){
 
 function toNumber(s){ //used in conversion functions
     var s = String(s);
-    s = s.replaceAll(" ", ""); //remove blanks
+    s = s.replaceAll(" ", "");
     if (s == ''){
         return 0;
     }
-
-    //handle fraction
-    if (s.includes("/")){
-        var arr = s.split("/");
-        var result = arr[0]/arr[1];
-        return result;
-    }
-
-    if (typeof(s) == 'string'){ //convert
+    if (typeof(s) == 'string'){
         return parseFloat(s);
     }
-
 }
 
 function ftInToMeter(ftIn){
@@ -94,27 +85,27 @@ function miToKm(mi){
 }
 
 function tbspToCup(tbsp){
-    return toNumber(tbsp)/16;
+    return tbsp/16;
 }
 
 function tbspToTsp(tbsp){
-    return toNumber(tbsp) * 3;
+    return tbsp * 3;
 }
 
 function cupToTbsp(cup){
-    return toNumber(cup) * 16;
+    return cup * 16;
 }
 
 function cupToTsp(cup){
-    return toNumber(cup) * 48;
+    return cup * 48;
 }
 
 function tspToTbsp(tsp){
-    return toNumber(tsp)/3;
+    return tsp/3;
 }
 
 function tspToCup(tsp){
-    return toNumber(tsp)/48;
+    return tsp/48;
 }
 
 //IDs of inputs 
@@ -122,72 +113,42 @@ var inpArr = ['cmInp', 'inchInp', 'meterInp', 'feetInp', 'feetInp2', 'inchInp2',
 
 //event listeners
 for (let i = 0; i<inpArr.length; i++){
-    document.getElementById(inpArr[i]).addEventListener("change", fromAny);    
-}
-
-for (let i = 0; i<inpArr.length; i++){
     document.getElementById(inpArr[i]).addEventListener("keyup", fromAny);    
 }
 
-//needed - correlate input field to array of array of functions
-var funcArr = [['cmToIn'], ['inToCm'], ['mToFt', 'meterToFtIn'], ['feetToMeter', 'feetToFtIn'], ['feetInp2FuncPlaceholder'], 
-['inchInp2FuncPlaceholder'], ['kmToMi'], ['miToKm'], ['tbspToCup', 'tbspToTsp'], ['cupToTbsp', 'cupToTsp'], 
-['tspToTbsp', 'tspToCup']]; //2d 
-var outArr = [['inchInp'], ['cmInp'], ['feetInp'], ['meterInp'], ['feetInp2OutputPlaceholder'], ['InchInp2OutputPlaceholder'], 
-['miInp'], ['kmInp'], ['cupInp', 'tspInp'], ['tbspInp', 'tspInp'], ['tbspInp', 'cupInp'] ]; //2d
-
-function fromFtIn(){ //trigger on ft or inch input , must precede fromAny
-    var ft = getVal('feetInp2');
-        var inch = getVal('inchInp2');
-        if (ft == "" && inch == ""){ //empty input => empty output
-            setVal('meterInp', ""); 
-            setVal('feetInp', "");    
-        }
-        else{
-            var meter = ftInToMeter([ft, inch]); //get meters
-            var feet = ftInToFeet([ft, inch]);
-            setVal('meterInp', meter); 
-            setVal('feetInp', feet);
-        }
-}
-
-function outputFtIn(){ //must precede fromAny
-    if (emptyInp){
-        setVal('feetInp2', "");
-        setVal('inchInp2', "");    
-    }
-    else{
-        var func = funcArrUse[i];
-        var ftInArr = window[func](inp);
-        setVal('feetInp2', ftInArr[0]);
-        setVal('inchInp2', ftInArr[1]);
-    }  
-}
-
-function fromAny(){ //triggered on any input's keyup    
+function fromAny(){ //triggered on any input's keyup
+    //needed - correlate input field to array of array of functions
+    var funcArr = [['cmToIn'], ['inToCm'], ['mToFt', 'meterToFtIn'], ['feetToMeter', 'feetToFtIn'], ['feetInp2FuncPlaceholder'], 
+    ['inchInp2FuncPlaceholder'], ['kmToMi'], ['miToKm'], ['tbspToCup', 'tbspToTsp'], ['cupToTbsp', 'cupToTsp'], 
+    ['tspToTbsp', 'tspToCup']]; //2d 
+    var outArr = [['inchInp'], ['cmInp'], ['feetInp'], ['meterInp'], ['feetInp2OutputPlaceholder'], ['InchInp2OutputPlaceholder'], 
+    ['miInp'], ['kmInp'], ['cupInp', 'tspInp'], ['tbspInp', 'tspInp'], ['tbspInp', 'cupInp'] ]; //2d
+    
     //handling based on inpArr > index > funcArr > outArr
     var inp = getVal(this.id);
-    var emptyInp = (inp == "") ? true:false;
     var index = inpArr.indexOf(this.id); //need index
     var functionCount = funcArr[index].length; //need count of all applicable functions 
     var funcArrUse = funcArr[index]; //useful functions
     var outArrUse = outArr[index]; //output locations
 
-    if (this.id == 'feetInp2' || this.id == 'inchInp2'){ //input is ft in 
-        fromFtIn();
+    if (this.id == 'feetInp2' || this.id == 'inchInp2'){
+        var ft = getVal('feetInp2');
+        var inch = getVal('inchInp2');
+        var meter = ftInToMeter([ft, inch]); //get meters
+        setVal('meterInp', meter); 
+        var feet = ftInToFeet([ft, inch]);
+        setVal('feetInp', feet);
     }
-    else{ 
+    else{
         for (let i = 0; i<functionCount; i++){
-            if (funcArrUse[i] == 'feetToFtIn' || funcArrUse[i] == 'meterToFtIn'){ //output is ft in 
-                outputFtIn();
+            if (funcArrUse[i] == 'feetToFtIn' || funcArrUse[i] == 'meterToFtIn'){
+                var func = funcArrUse[i];
+                var ftInArr = window[func](inp);
+                setVal('feetInp2', ftInArr[0]);
+                setVal('inchInp2', ftInArr[1]);
             }
             else {
-                if (emptyInp){
-                    setVal(outArrUse[i], "");    
-                }
-                else{
-                    setVal(outArrUse[i], window[funcArrUse[i]](inp));
-                }
+                setVal(outArrUse[i], window[funcArrUse[i]](inp));
             }
         }
     }
