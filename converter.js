db44fc06dfd1d2352fd6e2a9959596e100fe184f
Lina Li <61526855+echoeoe@users.noreply.github.com>
2023-05-03 02:54:02 -0400

    Update converter.js
    var removals

--- converter.js
+++ converter.js
@@ -16,17 +16,17 @@ function feetToMeter(feet){
 }
 
 function feetToFtIn(feet){
-    var ft = Math.floor(feet);
-    var inch = (feet - ft) * 12;
+    const ft = Math.floor(feet);
+    const inch = (feet - ft) * 12;
     return [ft, inch];
 }
 
 function meterToFtIn(meter){ 
     //convert to cm, then inches, then ft in 
-    var cm = meter * 100;
-    var inch = cm / 2.54;
-    var ftOnly = Math.floor(inch / 12);
-    var inchOnly = inch - (ftOnly * 12);
+    const cm = meter * 100;
+    const inch = cm / 2.54;
+    const ftOnly = Math.floor(inch / 12);
+    const inchOnly = inch - (ftOnly * 12);
     return [ftOnly, inchOnly];
 }
 
@@ -39,8 +39,8 @@ function toNumber(s){ //used in conversion functions
 
     //handle fraction
     if (s.includes("/")){
-        var arr = s.split("/");
-        var result = arr[0]/arr[1];
+        const arr = s.split("/");
+        const result = arr[0]/arr[1];
         return result;
     }
 
@@ -51,14 +51,14 @@ function toNumber(s){ //used in conversion functions
 }
 
 function ftInToMeter(ftIn){
-    var ft = ftIn[0];
-    var inch = ftIn[1];
+    let ft = ftIn[0];
+    let inch = ftIn[1];
 
     ft = toNumber(ft);
     inch = toNumber(inch);
     //convert to inches only, then cm, then m
-    var inchOnly = (ft * 12) + inch;
-    var cm = inchOnly * 2.54;
+    const inchOnly = (ft * 12) + inch;
+    const cm = inchOnly * 2.54;
     return m = cm / 100;
 }
 
@@ -72,24 +72,24 @@ function setVal(someID, val){
 
 function ftInToFeet(ftIn){
     //take array, convert to feet
-    var feetOnly = toNumber(ftIn[0]);
-    var inchOnly = toNumber(ftIn[1]);
-    var feet = feetOnly + (inchOnly / 12);
+    const feetOnly = toNumber(ftIn[0]);
+    const inchOnly = toNumber(ftIn[1]);
+    const feet = feetOnly + (inchOnly / 12);
     return feet;
 }
 
 function kmToMi(km){ // 1 mi = 63360 inches
     //km => cm => inches
-    var cm = km * 100000;
-    var inches = cm/2.54;
-    var mi = inches / 63360;
+    const cm = km * 100000;
+    const inches = cm/2.54;
+    const mi = inches / 63360;
     return mi;
 }
 
 function miToKm(mi){
-    var inches = mi * 63360;
-    var cm = inches * 2.54;
-    var km = cm / 100000;
+    const inches = mi * 63360;
+    const cm = inches * 2.54;
+    const km = cm / 100000;
     return km;
 }
 
@@ -118,7 +118,7 @@ function tspToCup(tsp){
 }
 
 //IDs of inputs 
-var inpArr = ['cmInp', 'inchInp', 'meterInp', 'feetInp', 'feetInp2', 'inchInp2', 'kmInp', 'miInp', 'tbspInp', 'cupInp', 'tspInp']; //1d
+const inpArr = ['cmInp', 'inchInp', 'meterInp', 'feetInp', 'feetInp2', 'inchInp2', 'kmInp', 'miInp', 'tbspInp', 'cupInp', 'tspInp']; //1d
 
 //event listeners
 for (let i = 0; i<inpArr.length; i++){
@@ -131,32 +131,32 @@ for (let i = 0; i<inpArr.length; i++){
 
 function fromAny(){ //triggered on any input's keyup
     //needed - correlate input field to array of array of functions
-    var funcArr = [['cmToIn'], ['inToCm'], ['mToFt', 'meterToFtIn'], ['feetToMeter', 'feetToFtIn'], ['feetInp2FuncPlaceholder'], 
+    const funcArr = [['cmToIn'], ['inToCm'], ['mToFt', 'meterToFtIn'], ['feetToMeter', 'feetToFtIn'], ['feetInp2FuncPlaceholder'], 
     ['inchInp2FuncPlaceholder'], ['kmToMi'], ['miToKm'], ['tbspToCup', 'tbspToTsp'], ['cupToTbsp', 'cupToTsp'], 
     ['tspToTbsp', 'tspToCup']]; //2d 
-    var outArr = [['inchInp'], ['cmInp'], ['feetInp'], ['meterInp'], ['feetInp2OutputPlaceholder'], ['InchInp2OutputPlaceholder'], 
+    const outArr = [['inchInp'], ['cmInp'], ['feetInp'], ['meterInp'], ['feetInp2OutputPlaceholder'], ['InchInp2OutputPlaceholder'], 
     ['miInp'], ['kmInp'], ['cupInp', 'tspInp'], ['tbspInp', 'tspInp'], ['tbspInp', 'cupInp'] ]; //2d
     
     //handling based on inpArr > index > funcArr > outArr
-    var inp = getVal(this.id);
-    var index = inpArr.indexOf(this.id); //need index
-    var functionCount = funcArr[index].length; //need count of all applicable functions 
-    var funcArrUse = funcArr[index]; //useful functions
-    var outArrUse = outArr[index]; //output locations
+    const inp = getVal(this.id);
+    const index = inpArr.indexOf(this.id); //need index
+    const functionCount = funcArr[index].length; //need count of all applicable functions 
+    const funcArrUse = funcArr[index]; //useful functions
+    const outArrUse = outArr[index]; //output locations
 
     if (this.id == 'feetInp2' || this.id == 'inchInp2'){
-        var ft = getVal('feetInp2');
-        var inch = getVal('inchInp2');
-        var meter = ftInToMeter([ft, inch]); //get meters
+        const ft = getVal('feetInp2');
+        const inch = getVal('inchInp2');
+        const meter = ftInToMeter([ft, inch]); //get meters
         setVal('meterInp', meter); 
-        var feet = ftInToFeet([ft, inch]);
+        const feet = ftInToFeet([ft, inch]);
         setVal('feetInp', feet);
     }
     else{
         for (let i = 0; i<functionCount; i++){
             if (funcArrUse[i] == 'feetToFtIn' || funcArrUse[i] == 'meterToFtIn'){
-                var func = funcArrUse[i];
-                var ftInArr = window[func](inp);
+                const func = funcArrUse[i];
+                const ftInArr = window[func](inp);
                 setVal('feetInp2', ftInArr[0]);
                 setVal('inchInp2', ftInArr[1]);
             }
@@ -167,5 +167,5 @@ function fromAny(){ //triggered on any input's keyup
     }
 }
 
-// module.exports = {cmToIn, inToCm, mToFt, feetToMeter, feetToFtIn, meterToFtIn, ftInToMeter, toNumber, ftInToFeet, kmToMi, 
-//     miToKm, tbspToCup, tbspToTsp, cupToTbsp, cupToTsp, tspToTbsp, tspToCup}; 
+module.exports = {cmToIn, inToCm, mToFt, feetToMeter, feetToFtIn, meterToFtIn, ftInToMeter, toNumber, ftInToFeet, kmToMi, 
+    miToKm, tbspToCup, tbspToTsp, cupToTbsp, cupToTsp, tspToTbsp, tspToCup}; 
